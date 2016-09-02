6631da0d0cb7fe6a0c72d10033dab378e5a9edc2
Santos Martin <santos.martingarca2@gmail.com>
2016-09-02 10:02:17 -0600

    se modifico la configuracion se agrego la linea estatica

--- misitio1/settings.py
+++ misitio1/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog', 
+    'blog',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -102,3 +102,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
